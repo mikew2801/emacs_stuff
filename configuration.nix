922b9ba7f5e025650d1d493efccc1e7c8895302d
Thomas Michael WIlliams <mikew2801@gmail.com>
2022-08-12 19:58:23 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,10 @@
       ./nixbox.nix
     ];
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
    hardware = {
