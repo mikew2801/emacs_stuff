ddaca8c4851a36426d55decd5a441510b27a816d
Thomas Williams <mikew2801@gmail.com>
2026-03-15 19:29:59 +0000

    Add files via upload

--- powernix.nix
+++ powernix.nix
@@ -1,32 +1,29 @@
 {config, pkgs, ...}:
 
 {
-networking.hostName = "powernix";
-
+networking.hostName = "mininix";
 
 services.xserver = {
     enable = true;
     #videoDrivers = lib.mkIf (config.networking.hostName == "nixbox") [ "amdgpu" ];
     #displayManager.gdm.enable = lib.mkIf (config.networking.hostName == "nixbox") true;
     #displayManager.lightdm.enable = lib.mkIf (config.networking.hostName == "workbox") true;
-    windowManager.stumpwm.enable = true;
 
+    windowManager.stumpwm.enable = true;
+    windowManager.exwm.enable = true;
+    #windowManager.qtile.enable = true;
+    windowManager.i3.enable = true;
+ 
     #windowManager.xmonad.enable = true;
     #windowManager.xmonad.enableContribAndExtras = true;
 
-    #windowManager.i3.enable = true;
+    #windowManager.i3.enable = true; 
     #windowManager.i3.package = pkgs.i3-gaps;
 
+    # Only one of cinnamon/gnome can be enabled without conflict
+
     desktopManager.cinnamon.enable = true;
-    
-    windowManager.exwm = {
-      enable = true;
-      enableDefaultConfig = false;
-      loadScript = "";
-      extraPackages = epkgs: [
-        epkgs.emacsql-sqlite
-    ];
-    };
-    #windowManager.openbox = { enable = true;};
+    desktopManager.xfce.enable = true;
+    #desktopManager.plasma6.enable = true;
 };
 }
